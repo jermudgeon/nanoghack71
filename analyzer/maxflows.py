6ed284b01566509d77f0b038e9c55c2d3a98c92f
John W. O'Brien <john@saltant.com>
2017-10-01 16:45:01 -0700

    Write out two static files, host-focused

--- analyzer/maxflows.py
+++ analyzer/maxflows.py
@@ -4,9 +4,13 @@ __author__ = "John W. O'Brien <obrienjw@upenn.edu>"
 
 
 import random
+import json
 
 import networkx
 import requests
+
+
+endpoint = 'host'
 
 
 state_names = [
@@ -26,37 +30,34 @@ def load_wedges(fname):
     return wedges
 
 
-def compute_max_flows():
+def compute_max_flows(wedges):
     G = networkx.DiGraph()
+    G.add_weighted_edges_from(wedges, weight='capacity')
 
+    # iterate over pairwise endpoints
+    max_flows = []
+    link_capacities = []
+    for u in G:
+        if endpoint not in u:
+            continue
+        for v in G:
+            if endpoint not in v or v == u:
+                continue
+            # compute max flow and flow dict
+            f, d = networkx.algorithms.flow.maximum_flow(G, u, v)
+
+            max_flows.append((u, v, f))
+            link_capacities.append((u, v, d))
+    return max_flows, link_capacities
+
+
+if __name__ == '__main__':
     # load current topology and capacities from DB
     state = random.choice(state_names)
     wedges = load_wedges(state + '.tsv')
 
-    G.add_weighted_edges_from(wedges, weight='capacity')
-
-    # iterate over pairwise endpoints
-    results = []
-    for u in G:
-        if 'leaf' not in u:
-            continue
-        for v in G:
-            if 'leaf' not in v or v == u:
-                continue
-            # compute max flow and flow dict
-            f, d = networkx.algorithms.flow.maximum_flow(G, u, v)
-            #print(u, v, f, d)
-
-            # XXX store results back to DB
-            """
-            Two tables:
-                1) timestamp, u, v, f
-                2) timestamp, u, v, d as str
-            """
-            results.append((u, v, f))
-    return results
-
-
-if __name__ == '__main__':
-    results = compute_max_flows()
-    print(results)
+    max_flows, capacities = compute_max_flows(wedges)
+    with open('www/latest.json', 'wt') as f:
+        f.write(json.dumps(max_flows))
+    with open('www/capacities.json', 'wt') as f:
+        f.write(json.dumps(capacities))
